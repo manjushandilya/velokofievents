3177d8a7f6cfcecad5ef19c6f6f4ef31688ea0f7
Manjunath Sahyanarayana <manjunath.sathyanarayana@gmail.com>
2022-05-20 17:52:44 +0530

    Configure login and logout

--- src/main/java/com/velokofi/events/security/SecurityConfigurer.java
+++ src/main/java/com/velokofi/events/security/SecurityConfigurer.java
@@ -4,14 +4,15 @@ import lombok.Getter;
 import lombok.Setter;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpStatus;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.builders.WebSecurity;
-import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.web.authentication.HttpStatusEntryPoint;
 import org.springframework.web.client.RestTemplate;
 
-@EnableWebSecurity
+//@EnableWebSecurity
 @Configuration
 @Getter
 @Setter
@@ -24,7 +25,7 @@ public class SecurityConfigurer extends WebSecurityConfigurerAdapter {
 
     @Override
     public void configure(final WebSecurity web) throws Exception {
-        web.ignoring().antMatchers("/resources/**", "/cache/**").anyRequest();
+        //web.ignoring().antMatchers("/resources/**", "/cache/**").anyRequest();
     }
 
     @Override
@@ -32,10 +33,14 @@ public class SecurityConfigurer extends WebSecurityConfigurerAdapter {
         // https://stackoverflow.com/questions/49273847/springboot-error-parsing-http-request-header
         http.headers().httpStrictTransportSecurity().disable();
 
-        http.authorizeRequests()
-                .anyRequest()
-                .authenticated()
-                .and()
+        http
+                .authorizeRequests(a -> a
+                        .antMatchers("/", "/error", "/webjars/**").permitAll()
+                        .anyRequest().authenticated()
+                )
+                .exceptionHandling(e -> e
+                        .authenticationEntryPoint(new HttpStatusEntryPoint(HttpStatus.UNAUTHORIZED))
+                )
                 .oauth2Login();
     }
 
