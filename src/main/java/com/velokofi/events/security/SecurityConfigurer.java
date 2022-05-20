2facd1f341d3a43167e636897f1d0548cd41872f
Manjunath Sahyanarayana <manjunath.sathyanarayana@gmail.com>
2022-05-20 18:25:16 +0530

    Configure static resources

--- src/main/java/com/velokofi/events/security/SecurityConfigurer.java
+++ src/main/java/com/velokofi/events/security/SecurityConfigurer.java
@@ -7,12 +7,10 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpStatus;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.web.authentication.HttpStatusEntryPoint;
 import org.springframework.web.client.RestTemplate;
 
-//@EnableWebSecurity
 @Configuration
 @Getter
 @Setter
@@ -24,18 +22,22 @@ public class SecurityConfigurer extends WebSecurityConfigurerAdapter {
     }
 
     @Override
-    public void configure(final WebSecurity web) throws Exception {
-        //web.ignoring().antMatchers("/resources/**", "/cache/**").anyRequest();
-    }
-
-    @Override
     protected void configure(final HttpSecurity http) throws Exception {
         // https://stackoverflow.com/questions/49273847/springboot-error-parsing-http-request-header
         http.headers().httpStrictTransportSecurity().disable();
 
         http
                 .authorizeRequests(a -> a
-                        .antMatchers("/", "/error", "/webjars/**").permitAll()
+                        .antMatchers(
+                                "/",
+                                "/error",
+                                "/webjars/**",
+                                "/css/**",
+                                "/image/**",
+                                "/js/**",
+                                "/vendor/**",
+                                "/favicon.ico"
+                        ).permitAll()
                         .anyRequest().authenticated()
                 )
                 .exceptionHandling(e -> e
